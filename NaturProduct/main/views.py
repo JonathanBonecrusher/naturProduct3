8c484adccbe7b062392d4e07df74d28d19c6e16a
marty <shootekb.2015@gmail.com>
2023-11-13 17:53:09 +0500

    k

--- NaturProduct/main/views.py
+++ NaturProduct/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homePageView(request):
-    return render(request, 'katalog.html')
+    return render(request, 'mainContent.html')
 
 def loginPageView(request):
     return render(request, 'login.html')
